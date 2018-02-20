a6fd0e70f47644c00f88c1c9f7e0648c452b3155
Matthew DiBello <matthew.quickbeam@gmail.com>
2018-02-20 14:06:54 -0500

    Polygon and polyline work

--- assignment1/eventHandler.js
+++ assignment1/eventHandler.js
@@ -66,25 +66,8 @@ window.onload = function() {
     return points;
   }
 
-  function pixelize(points) {
-    positions = [];
-    for (var i = 0; i < points.length; i+=2) {
-      x = points[i];
-      y = points[i+1];
-      x1 = x + 0.01;
-      y1 = y + 0.01;
-      positions = positions.concat([x1, y1, x, y1, x1, y, x, y]);
-    }
-    return positions;
-  }
-      
-  function constructLine() {
-    var x1 = currentPoints[2];
-    var y1 = currentPoints[5];
-    var x2 = currentPoints[10];
-    var y2 = currentPoints[13];
+  function linearGenerationTransform(x1, y1, x2, y2) {
     var m = (y2 - y1) / (x2 - x1);
-    var b = y2 - (m * x2);
     var points = [];
     if (m > 0) {
       if (m > 1) {
@@ -117,6 +100,27 @@ window.onload = function() {
         }
       }
     }
+    return points;
+  }
+
+  function pixelize(points) {
+    positions = [];
+    for (var i = 0; i < points.length; i+=2) {
+      x = points[i];
+      y = points[i+1];
+      x1 = x + 0.01;
+      y1 = y + 0.01;
+      positions = positions.concat([x1, y1, x, y1, x1, y, x, y]);
+    }
+    return positions;
+  }
+      
+  function constructLine() {
+    var x1 = currentPoints[2];
+    var y1 = currentPoints[5];
+    var x2 = currentPoints[10];
+    var y2 = currentPoints[13];
+    points = linearGenerationTransform(x1, y1, x2, y2);
     positions = pixelize(points);
     currentPoints = [];
     draw(currentView, positions);
@@ -135,6 +139,32 @@ window.onload = function() {
     }
     points = points.concat(linearGeneration([x1, y2], [x2, y2]));
     points = points.concat(linearGeneration([x2, y1], [x1, y1]));
+    positions = pixelize(points);
+    currentPoints = [];
+    draw(currentView, positions);
+  }
+
+  function constructPolygon() {
+    for (var i = 0; i < 8; i++) {
+      currentPoints.push(currentPoints[i]);
+    }
+    constructPolyline();
+  }
+
+  function constructPolyline() {
+    filteredPoints = [];
+    for (var offset = 0; offset < currentPoints.length; offset+=8) {
+      filteredPoints.push(currentPoints[offset+2]);
+      filteredPoints.push(currentPoints[offset+5]);
+    }
+    points = []
+    for (var offset = 0; offset < filteredPoints.length; offset+=2) {
+      x1 = filteredPoints[offset+0];
+      y1 = filteredPoints[offset+1];
+      x2 = filteredPoints[offset+2];
+      y2 = filteredPoints[offset+3];
+      points = points.concat(linearGenerationTransform(x1, y1, x2, y2));
+    }
     positions = pixelize(points);
     currentPoints = [];
     draw(currentView, positions);
@@ -191,10 +221,16 @@ window.onload = function() {
     addCurrentPoint(this, event);
     draw(currentView, currentPoints);
   }
+  document.getElementById('draw-polygon').onclick = function() {
+    constructPolygon();
+  }
 
   document.getElementById('canvas-polyline').onclick = function(event) {
     addCurrentPoint(this, event);
     draw(currentView, currentPoints);
+  }
+  document.getElementById('draw-polyline').onclick = function() {
+    constructPolyline();
   }
 
   var clears = document.querySelectorAll('#clear');
