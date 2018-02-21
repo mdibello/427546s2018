b5f2081af40e9d288ab8f73aaea71acfd4a98ae8
Matthew DiBello <matthew.quickbeam@gmail.com>
2018-02-21 17:14:33 -0500

    We're drawing some curves

--- assignment1/eventHandler.js
+++ assignment1/eventHandler.js
@@ -126,6 +126,37 @@ window.onload = function() {
     draw(currentView, positions);
   }
 
+  function constructCircle() {
+    var cx = currentPoints[2];
+    var cy = currentPoints[5];
+    var x1 = currentPoints[10];
+    var y1 = currentPoints[13];
+    var radius = Math.sqrt(Math.pow((cx-x1), 2) + Math.pow((cy-y1), 2));
+    var r_2 = Math.pow(radius, 2);
+    var xmax = cx + (radius / Math.SQRT2);
+    var points = [];
+    for (var x = cx; x < xmax; x+=0.01) {
+      y = cy + Math.sqrt(r_2 - Math.pow(x - cx, 2));
+      points.push(x);
+      points.push(y);
+      points.push(x);
+      points.push(points[points.length - 2]);
+      if (Math.abs(y - points[points.length - 1]) >
+        Math.abs(y - (points[points.length - 1] - 0.01))) {
+          points[points.length - 1] -= 0.01;
+      }
+      //points = points.concat([x,y, y,x, y,-x, x,-y, -x,-y, -y,-x, -y,x, -x,-y]);
+    }
+    var num_points = points.length;
+    for (var i = 0; i < num_points; i+=2) {
+      points.push(points[i+1]);
+      points.push(points[i]);
+    }
+    positions = pixelize(points);
+    currentPoints = [];
+    draw(currentView, positions);
+  }
+
   function constructRect() {
     var x1 = currentPoints[2];
     var y1 = currentPoints[5];
